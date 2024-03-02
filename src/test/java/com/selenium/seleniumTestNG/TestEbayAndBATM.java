e5d5595bbf1ce2c272fb530cca605bdd7f2d94a9
https://github.com/tomaszJablonski <tjablonski1990@gmail.com>
2024-03-02 23:37:11 +0100

    asserts

--- src/test/java/com/selenium/seleniumTestNG/TestEbayAndBATM.java
+++ src/test/java/com/selenium/seleniumTestNG/TestEbayAndBATM.java
@@ -2,18 +2,20 @@ package com.selenium.seleniumTestNG;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
 
-public class LoginTest {
+public class TestEbayAndBATM {
 
     //https://testng.org/#_annotations
+    //Before/After Test - wykona się tylko raz na początku/końcu
+    //Before/After Method - wykona sie za każdym razem gdy uruchomi się test case
 
     WebDriver driver;
 
     @BeforeTest
     public void setup() {
+        System.out.println("---Starts tests---");
         driver = WebDriverManager.edgedriver().create();
         driver.manage().window().maximize();
     }
@@ -21,6 +23,17 @@ public class LoginTest {
     @AfterTest
     public void quitBrowser() {
         driver.quit();
+        System.out.println("---End the tests---");
+    }
+
+    @BeforeMethod
+    public void exampleBeforeMethod(){
+        System.out.println("****exampleBeforeMethod****");
+    }
+
+    @AfterMethod
+    public void exampleAfterMethod(){
+        System.out.println("****exampleAfterMethod****");
     }
 
     @Test(priority = 2, testName = "login1", description = "test case XRay login1")
@@ -41,5 +54,13 @@ public class LoginTest {
     @Test(priority = 4, testName = "logout2")
     public void logoutTest2(){
         System.out.println("Logout");
+    }
+
+    @Test(priority = 5)
+    public void verifyTitle(){
+        String expectedTitle = "Electronics, Cars, Fashion, Collectibles & More | eBay";
+        driver.get("https://www.ebay.com/");
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
     }
 }
