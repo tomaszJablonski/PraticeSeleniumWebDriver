50ae66fbf544d5e45d8431cbffc476ac4d68cde9
https://github.com/tomaszJablonski <tjablonski1990@gmail.com>
2024-03-06 20:05:57 +0100

    parameters in testng xml

---
+++ src/test/java/com/selenium/seleniumTestNG/TestPassParameterFromTestNG.java
@@ -0,0 +1,59 @@
+package com.selenium.seleniumTestNG;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class TestPassParameterFromTestNG {
+
+    WebDriver driver;
+    //public static String browser = "edge"; // External configuration - XLS, CSV
+    @Parameters({"browser"})
+    @BeforeTest
+    public void setup(String browser) {
+        switch (browser) {
+            case "edge" -> {
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.setAcceptInsecureCerts(true);
+                driver = new EdgeDriver(edgeOptions);
+            }
+            case "chrome" -> {
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setAcceptInsecureCerts(true);
+                driver = new ChromeDriver(chromeOptions);
+            }
+            case "firefox" -> {
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setAcceptInsecureCerts(true);
+                driver = new FirefoxDriver(firefoxOptions);
+            }
+        }
+        driver.manage().window().maximize();
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+        driver.quit();
+    }
+
+    @Test
+    public void login() {
+        driver.get("https://www.saucedemo.com/");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+    }
+}
