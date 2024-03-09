dafc446d8736c3d9cb380f6f429a885403c0bfec
https://github.com/tomaszJablonski <tjablonski1990@gmail.com>
2024-03-09 06:52:40 +0100

    retry simple test and use Listeners for all test in testng.xml

--- src/test/java/com/selenium/seleniumTestNG/advanced/TestClassListenerDemo.java
+++ src/test/java/com/selenium/seleniumTestNG/advanced/TestClassListenerDemo.java
@@ -1,14 +1,25 @@
 package com.selenium.seleniumTestNG.advanced;
 
 import com.selenium.seleniumTestNG.advanced.commons.BaseClassTest;
+import com.selenium.seleniumTestNG.advanced.commons.Retry;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TestClassListenerDemo extends BaseClassTest {
 
-    @Test
+    //Retry for simple class use in Test
+    //For retry for all tests use Listeners in testng.xml
+    @Test(retryAnalyzer = Retry.class)
     public void launchApp(){
         driver.get("https://www.ebay.com/");
-        //only for training
-//        Assert.assertTrue(false);
+//        only for training
+        Assert.assertTrue(false);
+    }
+
+    @Test()
+    public void launchApp2(){
+        driver.get("https://www.ebay.com/");
+//        only for training
+        Assert.assertTrue(false);
     }
 }
---
+++ src/test/java/com/selenium/seleniumTestNG/advanced/commons/Retry.java
@@ -0,0 +1,18 @@
+package com.selenium.seleniumTestNG.advanced.commons;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+
+public class Retry implements IRetryAnalyzer {
+
+    private int retryCount = 0;
+    private static final int maxRetryCount = 3;
+
+    public boolean retry(ITestResult result) {
+        if (retryCount < maxRetryCount){
+            retryCount++;
+            return true;
+        }
+        return false;
+    }
+}
